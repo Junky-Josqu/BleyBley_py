df4c02193eb560d85c1a721752692a57a857be87
Teilzeitschwuchtel <jgonline@live.de>
2017-04-27 19:41:11 +0200

    add configs

--- game/launcher.py
+++ game/launcher.py
@@ -7,8 +7,8 @@ def show_module():
     x = module.file.read()
     return
 def set_module_native():
-    module.file.write(module.native)
+    module.file.write(module.native() )
 def set_module_viking():
-    module.file.write(module.viking_conquest)
+    module.file.write(module.viking_conquest() )
 def set_module_napoleonic():
-    module.file.write(module.napoleonic_wars)
+    module.file.write(module.napoleonic_wars() )
