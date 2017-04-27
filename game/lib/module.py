66a51c41aa82421dbac18e5c0774797c5534dc6a
Teilzeitschwuchtel <jgonline@live.de>
2017-04-27 16:10:20 +0200

    add native modules

---
+++ game/lib/module.py
@@ -0,0 +1,16 @@
+#For different Modules
+def file():
+    x = '~/.mbwarband/last_module_warband'
+    return x
+
+def native():
+    module = 'Native'
+    return module
+
+def viking_conquest():
+    module = 'Viking Conquest'
+    return module
+
+def napoleonic_wars():
+    module = 'Napoleonic Wars'
+    return module
